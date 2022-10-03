473cc085eae03cd311892e1b1f2e4890194beef7
RohithKKannan <hithu203@gmail.com>
2022-10-03 19:34:44 +0530

    Test commit

--- Assets/Scripts/Controls/masterControl.cs
+++ Assets/Scripts/Controls/masterControl.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This is the masterControlScript
+
 public class masterControl : MonoBehaviour
 {
     bool touchControl;
